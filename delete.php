a17974b18a082756319fa8eee83bbd37f7827ae5
llyzinski <42960684+llyzinski@users.noreply.github.com>
2020-11-04 11:22:30 +0100

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli("sql7.freemysqlhosting.net", "sql7373168", "tmSP9U89xx", "sql7373168");
+    $conn = new mysqli("sql7.freemysqlhosting.net", "sql7374422", "NmBYIP3fmp", "sql7374422");
     $id = $_POST['id'];
     $sql = "DELETE from lib_autor_tytul WHERE id='$id'";
     mysqli_query($conn, $sql);
